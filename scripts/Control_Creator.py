a79d19cbcc8ac23d5587bbfa73d842a3015e3691
Nicholas Dorland <11059821@uvu.edu>
2024-11-22 18:49:17 -0700

    Mild changes to the Control Creator
    Removed some commented lines and cleaned code a little.

--- scripts/Control_Creator.py
+++ scripts/Control_Creator.py
@@ -6,20 +6,17 @@ def create_controls():
     for obj in sels:
         #Get the transformation values of the object
         translation = cmds.xform(obj, q=True, t=True, ws=True)
-        orig_trans = cmds.xform(obj, q=True, t=True, os=True)
         rotation = cmds.xform(obj, q=True, ro=True, ws=True)
         rotation[2] = rotation[2] + 90
-        print(rotation)
         #Create a NURBS circle control
         base_name = obj.rpartition("_")[0]
         if (rotation==[0, 0, 90]):
-            print("Here!")
             rotation[1] = rotation[1] - 90
         ctrl_name = base_name + "_Ctrl"
         control = cmds.circle(n=ctrl_name, r=0.6)
         # Move and rotate the control to match the selected object 
-        cmds.xform(control, t=translation, worldSpace=True) 
-        cmds.xform(control, ro=rotation, worldSpace=True)
+        cmds.xform(control, t=translation, ws=True) 
+        cmds.xform(control, ro=rotation, ws=True)
         #Create a new group
         group_name = base_name + "_Grp"
         new_group = cmds.group(em=True, n=group_name)
@@ -28,9 +25,5 @@ def create_controls():
         cmds.xform(new_group, ro=rotation, ws=True)
         #Parent the object to the new group
         cmds.parent(control, new_group)
-        #Parent the selected object under the control 
-        #cmds.parent(obj, control)
-        #Output the group name for confirmation
-        print("Created group: " + new_group)
 #Run the function
 create_controls()
